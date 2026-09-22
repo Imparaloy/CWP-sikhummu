63d47aae161be0fb9eb89836efef679e9ad6d56d
Sirikul Khummung <66070192@kmitl.ac.th>
2026-09-22 11:19:20 +0700

    cell03/03

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,14 +1,9 @@
-import sys
-
-if len(sys.argv) > 1:
-    print("none")
-else:
-    i = 0
-    while i <= 10:
-        line = f"Table de {i}:"
-        j = 0
-        while j <= 10:
-            line += f" {i * j}"
-            j += 1
-        print(line)
-        i += 1
+i = 0
+while i <= 10:
+    line = f"Table de {i}:"
+    j = 0
+    while j <= 10:
+        line += f" {i * j}"
+        j += 1
+    print(line)
+    i += 1
